eb6eeeb60e3b1b3a33fd73b0252312269e858bc4
LesleyBonyo <lesleybonyo@gmail.com>
2025-02-25 15:21:58 +0300

    JS HTML DOM

--- Group A/11-02-2025/index.js
+++ Group A/11-02-2025/index.js
@@ -8,7 +8,7 @@ comment */
 // variables, identifiers and data types
 let firstName = "Jane"; //string
 let age = 15; //number
-let height = 5.5; //number
+let height = 5.5; //number - global scope
 let haveRegistered = true; //boolean
 let novalue = null; //null
 let secondName; //undefined
@@ -54,7 +54,151 @@ function helloUser(fullName){
 helloUser("Janet Addy");
 //return statement
 function getProduct(num1, num2){
-	return num1 * num2;
+	let num3 = 10; //local scope
+	return num1 * num2; // signify end of execution of a function
 }
+//console.log(num3); // error
+
 let ouProduct = getProduct(5,60);
 console.log(ouProduct);
+
+// function as an expression
+let helloWorld = function(){
+	console.log("Hello World");
+}
+helloWorld();
+
+//block scope
+{
+	const pi = 3.14; //block scope
+	var myName = "Joy";
+}
+//console.log(pi); //error
+console.log(myName);
+
+//Var
+var example = 13;
+var example = 'Jan';
+
+//let
+let secondExample = 13;
+//let secondExample = 'Jan'; //error
+
+//const
+const radius = 7;
+//radius = 10; //error
+
+secondExample = 45;
+//loops - for
+for(let i = 1; i < 5; i++){
+	console.log("Sentence " + i);
+}
+//while loop
+let j = 0;
+while(j <= 4){
+	console.log( "Text " + j);
+	j = j + 1;
+}
+//if..else
+let myAge = 14;
+if (myAge<18) {
+	console.log("Not allowed");
+}
+else if (myAge>18) {
+	console.log("Allowed");
+} else{
+	console.log("Perfect");
+}
+//switch..case
+let day = 3;
+switch(day){
+case 1:
+	console.log("Monday");
+	break;
+case 2:
+	console.log("Tuesday");
+	break;
+case 3:
+	console.log("Wednesday");
+	break;
+default:
+	console.log("Between Wed and Mon");
+}
+
+//DOM - methods
+//access html elements
+//by Id - getElementById()
+const firstParagraph = document.getElementById("par1");
+console.log(firstParagraph);
+
+//by class - getElementsByClassName()
+const myClass = document.getElementsByClassName("myclass"); //array
+console.log(myClass[0]);
+
+//by Tag - getElementsByTagName()
+const paragraphs = document.getElementsByTagName("p");
+
+//get elements by css selectors
+const heading = document.querySelector('.myclass');
+console.log(heading);
+
+//removing elements on the page - element.remove()
+heading.remove();
+
+//create elements on the page - document.createElement()
+let mydiv = document.getElementById("mydiv");
+const anotherhead = document.createElement('h2');
+anotherhead.textContent = "Tumaini High School";
+mydiv.appendChild(anotherhead);
+
+//properties
+//innerHTML - get or set content of a HTML element
+firstParagraph.innerHTML = "<b>We have changed this paragraph</b>";
+
+//textContent - get or set the text of a HTML element
+firstParagraph.textContent = "Make me bold";
+
+//get or set html attributes - element.attribute
+const myimg = document.getElementById('myimg');
+myimg.src = "https://th.bing.com/th/id/R.2b8e763cac892f3600a9c39e8c080798?rik=KE6r1yWXNeqmzw&pid=ImgRaw&r=0";
+
+//get or set css styles - element.style.cssproperty
+myimg.style.width = "50%";
+myimg.style.borderRadius = "50px";
+firstParagraph.style.textDecoration = "underline";
+anotherhead.style.border = "1px solid red";
+
+//get the value of an input element
+function findProduct(){
+	const number1 = document.getElementById('num1');
+	const number2 = document.getElementById('num2');
+	//get value
+	//number1.value
+	let prod = number1.value * number2.value;
+	document.getElementById('result').textContent = prod;
+	return;
+}
+
+function alertMe(){
+	alert("You have been alerted"); // gives an alert box
+}
+//events
+//add event as property
+const prodButton = document.getElementById("prodButton");
+//prodButton.click = findProduct();
+prodButton.onclick = alertMe();
+
+//add event listener
+//mouse event - onmouseover, onmouseout
+prodButton.addEventListener("mouseover", function(){
+	prodButton.style.backgroundColor = "Black";
+	prodButton.style.color = "white";
+});
+//create a function
+function mouseFunc(){
+	prodButton.style.backgroundColor = "white";
+	prodButton.style.color = "black";
+}
+prodButton.addEventListener("mouseout", mouseFunc);
+prodButton.addEventListener("click", findProduct);
+prodButton.addEventListener("click", alertMe);
--- Group B/12-02-2025/index.js
+++ Group B/12-02-2025/index.js
@@ -8,7 +8,7 @@ comment */
 //declaring variables
 let firstName = "John Jack"; //string use '' or ""
 let secondName; //undefined
-let age = 15; //number
+let age = 15; //number - global scope
 let height = 20.5; //number
 let isRegistered = false; //boolean
 let marks = null; //null
@@ -46,11 +46,165 @@ function hello(){
 hello();
 // function with parameters
 function helloStudent(studentName){
-	console.log("Hello " + studentName);
+	console.log("Hello " + studentName );
 }
 helloStudent("Clara Joy");
 function getProduct(num1, num2){
+	let example = "Local scope";
 	return num1 * num2; //signifies the end of execution of a function
 }
+//access variable with local scope
+//console.log(example); //give an error
+
 let ourProduct = getProduct(20, 4);
-console.log(ourProduct)
+console.log(ourProduct);
+
+//function as an expression
+let exampleFunction = function(){
+	console.log("Function as an expression");
+}
+exampleFunction();
+
+//block scope
+{
+	let myMarks = 50; // block scope
+	var yourMarks = 60;
+}
+//console.log(myMarks); //give an error
+//console.log(yourMarks);
+//var
+var yourVariable = true;
+var yourVariable = 15;
+
+//let 
+let someVariable = "Home";
+//let someVariable = 34; //give an error
+someVariable = 50;
+
+//const
+const pi = 3.14;
+
+//loops - for
+for(let i = 1; i < 4; i++){
+	console.log("Iteration " + i);
+}
+//while loop
+let j = 0;
+while(j <= 4){
+	console.log("Sentence " + j);
+	j = j + 1;
+}
+//if..else
+let voting = 20;
+if (voting<18) {
+	console.log("Not allowed");
+} else if (voting>18) {
+	console.log("Allowed");
+}
+else{
+	console.log("Finally you can vote");
+}
+//switch..case
+let dayOfWeek = 2;
+switch(dayOfWeek){
+case 1:
+	console.log("Monday");
+	break;
+case 2: 
+	console.log("Tuesday");
+	break;
+default:
+	console.log("Between Tue and Mon");
+}
+//DOM
+//access html elements
+//by Id - getElementById()
+const par1 = document.getElementById("par1");
+console.log(par1);
+
+//by class - getElementsByClassName()
+const ourClass = 
+		document.getElementsByClassName("class1");//array
+console.log(ourClass[1]);
+
+//by tag - getElementsByTagName()
+const paragraphs = document.getElementsByTagName("p");//array
+
+//using css selectors
+const heading = document.querySelector('.class1');
+console.log(heading);
+
+//remove/delete elements
+heading.remove();
+
+//create elements
+let newHeading = document.createElement('h1');
+//add some text to the heading
+newHeading.textContent = "This is a new heading";
+//pick the div on the page
+const newdiv = document.getElementById('newdiv');
+//add created heading to the div
+newdiv.appendChild(newHeading);
+
+//properties - used to get or change value of elements
+
+//innerHTML -get or change the content of elements
+par1.innerHTML = "<u>I have been changed</u>";
+
+newHeading.innerHTML = par1.innerHTML;
+
+//textContent - get or change the text content of elements
+ourClass[0].textContent = "A new link";
+
+//get or changing attributes of elements - element.nameofattribute
+ourClass[0].href = "https://strathmore.edu/";
+ourClass[0].target = "_blank";
+ourClass[0].title = "To strathmore site";
+
+const myimg = document.getElementById('myimg');
+myimg.src = "https://www.thespruce.com/thmb/JamFYX_T51P1oMAUZhOdd41GWQ4=/5128x3415/filters:no_upscale():max_bytes(150000):strip_icc()/livestock---sideview-of-a-boer-goat-kid-on-a-green-pasture---dodd-city--texas--usa--530704190-5c3fe72246e0fb00010eb949.jpg";
+
+//get or change css style - element.style.cssproperty
+myimg.style.width = "50%";
+myimg.style.borderRadius = "50px";
+newHeading.style.border = "1px solid red";
+
+//get value of form elements - value
+function findProduct(){
+	const num1 = document.getElementById('num1').value;
+	const num2 = document.getElementById('num2').value;
+	let ourProduct = num1 * num2;
+	const result = document.getElementById('result');
+	result.textContent = ourProduct;
+	result.style.color = "red";
+	return;
+}
+//events
+const productButton = 
+	document.getElementById("productButton");
+/*productButton.onclick = function(){
+	const num1 = document.getElementById('num1').value;
+	const num2 = document.getElementById('num2').value;
+	let ourProduct = num1 * num2;
+	const result = document.getElementById('result');
+	result.textContent = ourProduct;
+	result.style.color = "red";
+	return;
+};*/
+//addEventListener(event, function)
+productButton.addEventListener("click",findProduct);
+productButton.addEventListener("click",function(){
+	alert("Finding product");
+})
+//mouse events - mouseover, mouseout
+productButton.addEventListener("mouseover", function(){
+	productButton.style.color = "white";
+	productButton.style.backgroundColor = "Blue";
+	productButton.style.border = "none";
+});
+//mouseout
+productButton.addEventListener("mouseout", function(){
+	productButton.style.color = "white";
+	productButton.style.backgroundColor = "red";
+	productButton.style.border = "none";
+});
